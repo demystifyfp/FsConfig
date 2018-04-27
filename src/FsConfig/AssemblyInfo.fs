259e0eeda66443ab5bf199ef223e850e589ec9c0
Tamizhvendan S <tamizh88@gmail.com>
2018-04-27 12:17:19 +0530

    Bump version to 0.0.6

--- src/FsConfig/AssemblyInfo.fs
+++ src/FsConfig/AssemblyInfo.fs
@@ -5,8 +5,8 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FsConfig")>]
 [<assembly: AssemblyProductAttribute("FsConfig")>]
 [<assembly: AssemblyDescriptionAttribute("F# Libary for managing configuration data from environment variables and application config in a type-safe way")>]
-[<assembly: AssemblyVersionAttribute("0.0.5")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.5")>]
+[<assembly: AssemblyVersionAttribute("0.0.6")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.6")>]
 [<assembly: AssemblyConfigurationAttribute("Release")>]
 do ()
 
@@ -14,6 +14,6 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "FsConfig"
     let [<Literal>] AssemblyProduct = "FsConfig"
     let [<Literal>] AssemblyDescription = "F# Libary for managing configuration data from environment variables and application config in a type-safe way"
-    let [<Literal>] AssemblyVersion = "0.0.5"
-    let [<Literal>] AssemblyFileVersion = "0.0.5"
+    let [<Literal>] AssemblyVersion = "0.0.6"
+    let [<Literal>] AssemblyFileVersion = "0.0.6"
     let [<Literal>] AssemblyConfiguration = "Release"
