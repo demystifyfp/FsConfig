765656834d535f17ecfde21ff196614199b3bc64
queil <queil@users.noreply.github.com>
2021-01-09 19:51:47 +0000

    Fixed owner references

--- build.fsx
+++ build.fsx
@@ -72,7 +72,7 @@ let docsDir = __SOURCE_DIRECTORY__  @@ "docs"
 let docsSrcDir = __SOURCE_DIRECTORY__  @@ "docsSrc"
 let docsToolDir = __SOURCE_DIRECTORY__ @@ "docsTool"
 
-let gitOwner = "queil"
+let gitOwner = "demystifyfp"
 let gitRepoName = "FsConfig"
 
 let gitHubRepoUrl = sprintf "https://github.com/%s/%s" gitOwner gitRepoName
