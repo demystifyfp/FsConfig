b5512733ba450a9862b10e52f5409e0228aea1e8
Alessio Aurecchia <alessio@aurecchia.ch>
2021-07-25 10:47:41 +0200

    Add support for URIs. (#24)

--- src/FsConfig/Config.fs
+++ src/FsConfig/Config.fs
@@ -154,6 +154,11 @@ module internal Core =
       wrap (tryParseEnum<'T> enumShape)
     | Shape.FSharpUnion (:? ShapeFSharpUnion<'T> as shape) ->
       wrap (tryParseFSharpDU shape)
+    | Shape.Uri ->
+      wrap (fun s ->
+          try
+              Some (Uri s)
+          with _ -> None)
     | _ -> None
 
   let parseFSharpOption<'T> name value (fsharpOption : IShapeFSharpOption) =
