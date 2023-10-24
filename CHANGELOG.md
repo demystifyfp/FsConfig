1501371fe0e5de930802b41b1a7907119e321fbb
queil <queil@users.noreply.github.com>
2023-10-24 18:00:20 +0000

    Bump version to 4.1.0
    ## [4.1.0] - 2023-10-24
    
    ### Added
    
    - Support for optional subsections by [queil](https://github.com/queil)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [4.1.0] - 2023-10-24
+
+### Added
+
+- Support for optional subsections by [queil](https://github.com/queil)
+
 ## [4.0.0] - 2023-06-18
 
 ### Changed
@@ -167,5 +173,6 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 
 - Initial release
-[Unreleased]: https://github.com/demystifyfp/FsConfig/compare/v4.0.0...HEAD
+[Unreleased]: https://github.com/demystifyfp/FsConfig/compare/v4.1.0...HEAD
+[4.1.0]: https://github.com/demystifyfp/FsConfig/compare/v4.0.0...v4.1.0
 [4.0.0]: https://github.com/demystifyfp/FsConfig/compare/v3.1.0...v4.0.0
--- src/FsConfig/AssemblyInfo.fs
+++ src/FsConfig/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("FsConfig")>]
 [<assembly: AssemblyProductAttribute("FsConfig")>]
-[<assembly: AssemblyVersionAttribute("4.0.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2023-06-18T00:00:00.0000000+01:00")>]
-[<assembly: AssemblyFileVersionAttribute("4.0.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("4.0.0")>]
+[<assembly: AssemblyVersionAttribute("4.1.0")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2023-10-24T00:00:00.0000000+00:00")>]
+[<assembly: AssemblyFileVersionAttribute("4.1.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("4.1.0")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","247fcb0aa106a1934a3a151b6d2bfd716957f2a1")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","b1fbcfab8f27e0c13e5038a2bbd38b3911f8441a")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "FsConfig"
     let [<Literal>] AssemblyProduct = "FsConfig"
-    let [<Literal>] AssemblyVersion = "4.0.0"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2023-06-18T00:00:00.0000000+01:00"
-    let [<Literal>] AssemblyFileVersion = "4.0.0"
-    let [<Literal>] AssemblyInformationalVersion = "4.0.0"
+    let [<Literal>] AssemblyVersion = "4.1.0"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2023-10-24T00:00:00.0000000+00:00"
+    let [<Literal>] AssemblyFileVersion = "4.1.0"
+    let [<Literal>] AssemblyInformationalVersion = "4.1.0"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "247fcb0aa106a1934a3a151b6d2bfd716957f2a1"
+    let [<Literal>] AssemblyMetadata_GitHash = "b1fbcfab8f27e0c13e5038a2bbd38b3911f8441a"
